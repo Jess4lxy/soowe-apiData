906e91a0c14d9fb8f23a3ec028d5755d99e64c2b
Jess4lxy <alex.mtz.rv17@gmail.com>
2025-01-28 13:43:45 -0600

    cloudinary ready

---
+++ src/utils/cloudinaryUpload.ts
@@ -0,0 +1,15 @@
+import cloudinary from "../config/cloudinaryConfig";
+
+export const subirPerfil = async (rutaLocal: string): Promise<string> => {
+    try {
+        const resultado = await cloudinary.uploader.upload(rutaLocal, {
+            folder: 'soowie/perfiles', // folder
+            allowed_formats: ['jpg', 'jpeg', 'png'],
+            transformation: [{ width: 200, height: 200, crop: 'fill' }]
+        });
+        return resultado.secure_url; // this is the public URL of the image
+    } catch (error) {
+        console.error('Error al subir imagen de perfil:', error);
+        throw error;
+    }
+};
